198268f347fad8eea6ac4a3130e7d6b4e290c95f
Belbachiraimene3 <Belbachiraimene3@gmail.com>
2021-03-11 20:30:03 -0500

    New file git added

---
+++ src/TestGit/GIT.java
@@ -0,0 +1,14 @@
+package TestGit;
+
+
+public class GIT {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is the Git");
+		
+		System.out.println("we are learning Git");
+	}
+
+}
