21c4ab59e883894721234e2c378e5becc04d2354
Belbachiraimene3 <Belbachiraimene3@gmail.com>
2021-03-21 13:33:20 -0400

    upsate

---
+++ src/StringBuilder.java
@@ -0,0 +1,17 @@
+
+public class StringBuilder {
+
+	public StringBuilder(String var1) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String var1="Nelson";
+		StringBuilder var=new StringBuilder (var1);
+		System.out.println(var);
+		
+		
+	}
+
+}
